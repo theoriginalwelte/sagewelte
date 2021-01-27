a4ec332683b0ee7399561e6ce35ca4c8e61a6523
theoriginalwelte <welte.sage@gmail.com>
2021-01-27 15:42:45 -0500

    1/27/21 15:42

---
+++ sagewelte/components/Maps.vue
@@ -0,0 +1,26 @@
+<template>
+	<div id="maps">
+		<BuffaloBeltLine></BuffaloBeltLine>
+	</div>
+</template>
+
+<script>
+import BuffaloBeltLine from "./maps/BuffaloBeltLine.vue";
+
+export default {
+	data() {
+		return {};
+	},
+	name: "Maps",
+	components: {
+		BuffaloBeltLine,
+	},
+};
+</script>
+
+<style scoped>
+#maps {
+	margin-left: 21%;
+	width: 78%;
+}
+</style>
---
+++ sagewelte/components/maps/BuffaloBeltLine.vue
@@ -0,0 +1,52 @@
+<template>
+	<div>
+		<h1>Buffalo Belt Line • Reformed</h1>
+		<vl-map
+			:load-tiles-while-animating="true"
+			:load-tiles-while-interacting="true"
+			style="height: 575px;"
+			data-projection="EPSG:3857"
+		>
+			<vl-view
+				:zoom.sync="zoom"
+				:center.sync="center"
+				:rotation.sync="rotation"
+			></vl-view>
+
+			<vl-layer-tile>
+				<vl-source-osm></vl-source-osm>
+			</vl-layer-tile>
+
+			<vl-layer-vector>
+				<vl-source-vector :url="url">
+					<vl-style-box>
+						<vl-style-circle :radius="5">
+							<vl-style-fill color="#FFFFFF"></vl-style-fill>
+							<vl-stile-stroke color="#000000"></vl-stile-stroke>
+						</vl-style-circle>
+					</vl-style-box>
+				</vl-source-vector>
+			</vl-layer-vector>
+		</vl-map>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			url: "../../src/maps/buffalo-belt-line/buffalo_rail_extension.geojson",
+			zoom: 13,
+			center: [-8779318.203122, 5298450.17],
+			rotation: 0,
+		};
+	},
+};
+</script>
+
+<style scoped>
+h1 {
+	font-family: "Nunito", sans-serif;
+	font-weight: 200;
+}
+</style>
